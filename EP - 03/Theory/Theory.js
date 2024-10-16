1b3755b94f870169845aaea33e161a210a88c518
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-10-17 02:37:57 +0530

    Theory file Added

---
+++ EP - 03/Theory/Theory.js
@@ -0,0 +1,26 @@
+// If you want to know how to run the project for development build and production build, we have to go to the "scripts" section of package.json
+
+
+/* 
+const heading = React.createElement(
+    "h1",{ id:"heading"},"Hello World"
+);
+console.log(heading);
+
+
+
+
+const jsxHeading = <h1>React</h1>
+console.log(jsxHeading);
+
+
+
+- Both of them will print the same object
+*/
+
+
+// JSX(transpiled) => Babel transpiles it to React.createElement => React Element => JS Object => HTML Element(render)
+
+
+
+//JSX is different than "HTML in JS" . In HTML, we use "class" as attribute but in JSX , class is taken as a keyword, so we use "className".
